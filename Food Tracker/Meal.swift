9f3a0f597a9dbcb1f6e8be413b942b95138395c4
ramsandhya <ram.sandhya@gmail.com>
2016-09-22 12:42:21 -0400

    Implemented coder and decoder for data stotage and access

--- Food Tracker/Meal.swift
+++ Food Tracker/Meal.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let nameKey = "X"
+        static let photoKey = "Y"
+        static let ratingKey = "Z"
+    }
     
     // MARK: Initializer
     init?(name: String, photo: UIImage?, rating: Int){
@@ -25,7 +32,26 @@ class Meal {
             return nil
         }
     }
+    
+    // MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(photo, forKey: PropertyKey.photoKey)
+        aCoder.encode(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder: NSCoder){
+        let name = coder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = coder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+        
+        let rating = coder.decodeInteger(forKey: PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
 
+    }
 }
 
 
